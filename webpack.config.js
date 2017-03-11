756e7208e164bc5b59a695f6b02ba15f53ec27f1
Steven <steven@ceriously.com>
2017-03-11 18:05:13 -0500

    Almost working with two configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const { CheckerPlugin } = require('awesome-typescript-loader')
 
-module.exports = {
-    entry: "./src/browser.tsx",
+module.exports = [{
+    entry: {
+        "browser": "./src/browser.tsx"
+    },
     output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
+        path: __dirname + "/dist",
+        filename: "[name].js"
     },
     // Currently we need to add '.ts' to the resolve.extensions array.
     resolve: {
@@ -25,5 +27,39 @@ module.exports = {
     },
     plugins: [
         new CheckerPlugin()
-    ]
-};
+    ],
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+},
+{
+    entry: {
+        "server": "./src/server.tsx"
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].js"
+    },
+    // Currently we need to add '.ts' to the resolve.extensions array.
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+
+    // Source maps support ('inline-source-map' also works)
+    devtool: 'source-map',
+
+    // Add the loader for .ts files.
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CheckerPlugin()
+    ],
+    target: "node"
+}];
